57c37bd963975d9500acfe904574ac152cccace5
Clark Nicolas <clarkonicolas@gmail.com>
2016-10-25 11:01:24 +0000

    Bootstrap support + Ugly Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/root', to: 'user_pages#home'
+  get '/result', to: 'user_pages#result'
+  get '/support', to: 'user_pages#support'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'user_pages#home'
+  
 end
