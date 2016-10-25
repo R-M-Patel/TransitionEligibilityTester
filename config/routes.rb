6c46db28930d7e869239ef6e5e9f35155ed981f5
Clark Nicolas <clarkonicolas@gmail.com>
2016-10-25 11:45:57 +0000

    heroku why must you hurt me p3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root 'user_pages#home'
   get '/root', to: 'user_pages#home'
   get '/result', to: 'user_pages#result'
   get '/support', to: 'user_pages#support'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'user_pages#home'
+  
 #   root 'application#hello'
   
 end
