a81bdb5fcc77b56a0bcb3363a51b60ffdcbeea5c
Justin Piper <jpiper4198@gmail.com>
2016-11-09 11:55:51 -0500

    I think I fixed it :')

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,44 +1,22 @@
 class UserController < ApplicationController
-	def list
-		@users = User.all
-	end
-	
-	def show
-		@user = User.find(params[:id])
-	end
-	
-	def new
-		@user = User.new
-	end
-	
-	def create
-		@user = User.new(user_params)
-		
-		if @user.save
-			redirect_to :action => 'list'
-		else
-			render :action => 'new'
-		end
-	end
-	
-	def edit
-		@user = User.find(params[:id])
-	end
-	
-	def update
-		@user = User.find(params[:id])
-		
-		if @user.update_attrubutes(user_params)
-			redirect_to :action => 'show', :id => @user
-		else
-			render :action => 'edit'
-		end
-	end
-	
-	def delete
-		User.find(params[:id]).destroy
-		redirect_to :action => 'list'
-	end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 	
 	def panel
 		@user = User.find(params[:id])
@@ -47,9 +25,15 @@ class UserController < ApplicationController
 		
 		@program_requirements = ProgramRequirement.new
 	
+	end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
+	
+	
 
-def user_params
-	params.require(:user).permit(:first_name, :last_name, :email, :password, :administrator)
-end
 	
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,9 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-	validates :first_name, presence: true
-	validates :last_name, presence: true
-	validates :password, presence: true
-	validates :email, presence: true
-=======
+
     before_save {email.downcase!}
     validates :first_name, presence: true
     validates :last_name, presence: true
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}
->>>>>>> admin-login
+
 end
